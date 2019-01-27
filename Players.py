0a2fa0fd7a0414fad53413145bbcc404a27f37f3
bridgitt ku <bridgittku@237.240.21.198.tigernet.wifi.dyn.clemson.edu>
2019-01-27 02:07:31 -0500

    Players

---
+++ Players.py
@@ -0,0 +1,37 @@
+import json
+import random
+from pprint import pprint
+
+
+weight = 77
+height = 1.88
+aWeight = (weight-60.3278)/.275
+aHeight = (height-1.7)/.06713
+
+
+#print(aWeight)
+#print(aHeight)
+with open('pokemon.json') as f:
+    pokemon = json.load(f)
+    count = 0
+    approvedPokemon = []
+    for i in pokemon:
+        if count <= 1008:
+            if abs(pokemon[i]['heightm']-aHeight) < 1:
+                if abs(pokemon[i]['weightkg']-aWeight) < 1:
+                    approvedPokemon.append(i)
+        count += 1
+    #pprint(approvedPokemon)
+    #print(len(approvedPokemon))
+
+with open('players.json') as f:
+    players = json.load(f)
+    count = 0
+    for x in players:
+        while count <= len(players):
+        #pprint(players['players'][0]['hgt'])
+            pprint(players['players'][count]['hgt'])
+            count += 1
+
+
+
